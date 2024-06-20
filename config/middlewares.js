e91f06ae8821608a3d3c626dfb023b48180e7e46
Carlos Orjuela <orjuelacarlos80@gmail.com>
2024-06-20 16:13:22 -0500

    strapi configurado con su s3

---
+++ config/middlewares.js
@@ -0,0 +1,39 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': [
+              "'self'", 
+              'data:', 
+              'blob:', 
+              'market-assets.strapi.io', 
+              'https://zephyra-next-s3.s3.us-east-2.amazonaws.com'
+            ],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              'https://zephyra-next-s3.s3.us-east-2.amazonaws.com',
+            ],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
